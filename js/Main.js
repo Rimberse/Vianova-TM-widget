b1330e2ed9f7b2518251e714a5c4518832093455
Rimberse <kerimpltm@gmail.com>
2022-05-12 15:14:55 +0200

    Initial commit

---
+++ js/Main.js
@@ -0,0 +1,55 @@
+define
+(
+    'Widget/js/Main',
+    [
+        // 3DEXPERIENCE Cloud Platform JS modules 
+        'DS/PlatformAPI/PlatformAPI',
+
+        // Module which requests data and loads them into the application
+        'Modules/DataLoader'
+    ],
+    function
+    (
+        PlatformAPI,
+        DataLoader
+    )
+    {
+        // Declare public functions or variables here. Accessible by other modules. Call it by "Main.<function>". Usage sample, e.g. Main.onLoad() 
+        var exports = {
+            onLoad: function() {
+                console.info("Global var widget", widget);
+
+                console.info("Widget is running!");
+
+                widget.body.innerHTML = '';
+                widget.body.appendChild(DataLoader.displayPopup());
+                DataLoader.displayCredentials();
+
+                const container = document.createElement('div');
+                widget.body.appendChild(container);
+            },
+
+            onResize: function() {
+                console.info("onResize");
+            },
+
+            onViewChange: function() {
+                console.info("onViewChange");
+            },
+
+            onEdit: function() {
+                console.info("onEdit");
+            },
+
+            onRefresh: function() {
+                console.info("onRefresh");
+            },
+
+            endEdit: function() {
+                console.info("endEdit");
+            }
+        };
+
+        return exports;
+    }
+);
---
+++ modules/DataLoader.js
@@ -0,0 +1,90 @@
+define
+    (
+        "Modules/DataLoader",
+        [
+
+        ],
+        function
+            (
+
+            ) {
+            console.info('Data loader is loaded');
+
+            // global variables (widget, UWA, document)
+            let accessToken;
+            let username;
+            let password;
+            // Vianova's API
+            const host = 'https://api.vianova.dev';
+
+            // Leaflet map;
+            let map;
+            // Application's state, used to prevent from calling the web service twice
+            const state = {};
+
+            // Private functions. Not accessible by other modules. Accesible within this module only
+
+            // Creates a popup to retrieve credentials
+            // Used to retrieve Vianova credentials and token, required for later use
+            function getCredentials() {
+                var popup = document.createElement('div');
+                popup.id = "popup";
+
+                var accessTokenDiv = document.createElement('div');
+                accessTokenDiv.innerHTML = "Mapbox access token:"
+                var inputTkn = document.createElement('input');
+                inputTkn.id = "tkn";
+                inputTkn.type = "text";
+
+                var usernameDiv = document.createElement('div');
+                usernameDiv.innerHTML = "Username:"
+                var inputUsr = document.createElement('input');
+                inputUsr.id = "usr";
+                inputUsr.type = "text";
+
+                var passwordDiv = document.createElement('div');
+                passwordDiv.innerHTML = "Password:"
+                var inputPass = document.createElement('input');
+                inputPass.id = "pass";
+                inputPass.type = "password";
+
+                var confirmBtn = document.createElement('button');
+                confirmBtn.id = "confirm-btn";
+                confirmBtn.role = "button";
+                confirmBtn.innerHTML = "Confirm";
+                confirmBtn.onclick = function done() {
+                    document.getElementById("popup").style.display = "none";
+                    var token = document.getElementById("tkn").value;
+                    var usr = document.getElementById("usr").value;
+                    var pass = document.getElementById("pass").value;
+
+                    console.log(token, usr, pass);
+                    accessToken = token;
+                    username = usr;
+                    password = pass;
+                };
+
+                popup.appendChild(accessTokenDiv);
+                popup.appendChild(inputTkn);
+                popup.appendChild(usernameDiv);
+                popup.appendChild(inputUsr);
+                popup.appendChild(passwordDiv);
+                popup.appendChild(inputPass);
+                popup.appendChild(confirmBtn);
+                return popup;
+            }
+
+            // Vianova's API requests
+
+            // Loads Leaflet with Mapbox map
+
+            // Declare public functions or variable here. Accessible by other modules.
+            var exports = {
+                displayPopup: function () {
+                    return getCredentials();
+                }
+            };
+
+            return exports;
+        }
+    );
