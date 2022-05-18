b9752243a0b5b0ab5cbb72cd061ed854e2851420
Rimberse <kerimpltm@gmail.com>
2022-05-18 16:28:35 +0200

    Testing...

--- js/Main.js
+++ js/Main.js
@@ -50,19 +50,28 @@ define
                     console.info(usr, pass, body.toString());
                     console.info(encodeURIComponent('username') + '=' + encodeURIComponent(usr) + '&' + encodeURIComponent('password') + '=' + encodeURIComponent(pass));
 
+                    WAFData.proxifiedRequest('https://httpbin.org/get', {
+                        method: 'GET',
+                        onComplete: function (responseAsString) {
+                            console.info(responseAsString);
+                        },
+                        onFailure: function (error, responseAsString) {
+                            console.info('Couldnt retrieve info: ' + error + '\nResponse: ' + JSON.stringify(responseAsString));
+                        }
+                    });
+
                     WAFData.proxifiedRequest(address, {
                         method: 'POST',
                         type   : 'json',
                         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
                         data: encodeURIComponent('username') + '=' + encodeURIComponent(usr) + '&' + encodeURIComponent('password') + '=' + encodeURIComponent(pass),
                         onComplete: function (responseAsString) {
-                            // console.info(JSON.parse(responseAsString));
                             console.info(responseAsString);
                             let data = (async () => await rawResponse.json())();
                             console.info(data);
                             console.info(data['access_token']);
                         },
-                        onFailure: function (error, responseAsString) { 
+                        onFailure: function (error, responseAsString) {
                             console.info('Couldnt retrieve token: ' + error + '\nResponse: ' + JSON.stringify(responseAsString));
                         }
                     });
